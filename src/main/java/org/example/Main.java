5afdab26088f8306421533c41d10b4d1236284aa
Desi <desixd107@gmail.com>
2024-02-09 08:57:22 +0100

    add dividir

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,17 @@ package org.example;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+    }
+    public static int dividir(int dividendo, int divisor) {
+        // Manejo de la división por cero
+        if (divisor == 0) {
+            System.out.println("Error: No se puede dividir por cero.");
+            return Integer.MIN_VALUE; // Devuelve el valor mínimo de un entero para indicar un resultado indefinido
         }
+
+        // Realizar la división
+        int cociente = dividendo / divisor;
+        return cociente;
     }
 }
